d69a29932d8f20c313aba6e13ce0fefc32cd12d8
Lovenson2000 <beaucicotlovenson@gmail.com>
2023-08-19 21:23:37 +0800

    Handling scrolling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
 import Navbar from './components/navbar/Navbar.js';
 import Home from './components/home/Home.js';
 import About from './components/about/About.js';
@@ -8,8 +8,10 @@ import Contact from './components/contact/Contact.js';
 import Footer from './components/footer/Footer.js'
 
 export default function App() {
+
     return (
         <Router>
+            <ScrollToTop />
             <div className='app'>
                 <Navbar />
                 <Routes>
@@ -22,4 +24,14 @@ export default function App() {
             </div>
         </Router>
     );
+}
+//Function to scroll to top at every navigation change
+function ScrollToTop () {
+    const locations = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [locations]);
+
+    return null;
 }
